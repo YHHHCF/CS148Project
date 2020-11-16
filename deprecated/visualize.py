1a76484bb8f5e81d2dc58d8e5546eda08e3ee355
Erbao <erbao_huang@apple.com>
2020-11-16 00:01:25 -0800

    visualize first implementation of photon map

---
+++ deprecated/visualize.py
@@ -0,0 +1,33 @@
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+# Show all the photons' locations in the npy file
+# Plot in matplotlib, used for debug
+def visualize(path):
+    locations = np.load(path)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    print(f'Visualize{locations.shape[0]: d} locations.')
+    x = locations[:, 0]
+    y = locations[:, 1]
+    z = locations[:, 2]
+
+    ax.scatter(x, y, z, s=0.01, c='r', marker='o')
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
+
+
+# Translate the photon map file to a ply file
+# Can be loaded in Blender for better visaulization
+def save_as_ply(path):
+    pass
+
+if __name__ == "__main__":
+	visualize("../profile/locations.npy")
--- tools/visualize.py
+++ tools/visualize.py
@@ -1,33 +1,44 @@
+# Debug tools to convert photon points in the map to PLY file
+# Visualize in either blender or pyntcloud
+# PLY writer from https://github.com/dranjan/python-plyfile
+# Point cloud visualizer from https://github.com/daavoo/pyntcloud
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
+from plyfile import PlyData, PlyElement
+from pyntcloud import PyntCloud
 
-# Show all the photons' locations in the npy file
-# Plot in matplotlib, used for debug
-def visualize(path):
-    locations = np.load(path)
+import sys
+sys.path.append("../")
+from photonMap import PhotonMap
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    print(f'Visualize{locations.shape[0]: d} locations.')
-    x = locations[:, 0]
-    y = locations[:, 1]
-    z = locations[:, 2]
-
-    ax.scatter(x, y, z, s=0.01, c='r', marker='o')
-
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-
-    plt.show()
-
-
-# Translate the photon map file to a ply file
+# Convert the photon map photon locations to a PLY file
 # Can be loaded in Blender for better visaulization
-def save_as_ply(path):
-    pass
+def convert_map_to_ply(map_path, ply_path, depth_filter=None):
+    photon_map = PhotonMap(map_path)
+    locs = np.array([],dtype=[('x','f4'), ('y','f4'),('z','f4')])
+
+    for i in range(len(photon_map.map)):
+        # Can only save photons with specific depth
+        if depth_filter:
+            if photon_map.map[i].depth != depth_filter:
+                continue
+
+        l = photon_map.map[i].location
+        loc = np.array([(l[0], l[1], l[2])], \
+        dtype=[('x','f4'), ('y','f4'),('z','f4')])
+        locs = np.append(locs, loc)
+    desc = PlyElement.describe(locs, 'vertex')
+    PlyData([desc], text=True).write(ply_path)
+
+# Show all the photons' locations in the PLY file
+# Can also visulize PLY file in Blender using the following tool
+# https://github.com/uhlik/bpy/blob/master/space_view3d_point_cloud_visualizer.py
+# https://www.youtube.com/watch?v=eXct_7k779Q
+def visualize_ply(ply_path):
+    plydata = PyntCloud.from_file(ply_path)
+    plydata.plot()
 
 if __name__ == "__main__":
-	visualize("../profile/locations.npy")
+    map_path = "../profile/SingleMap.pkl"
+    ply_path = "../profile/SingleMap.ply"
+    convert_map_to_ply(map_path, ply_path)
+    visualize_ply(ply_path)
